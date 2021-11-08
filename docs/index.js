81ce22e4589563c98085ce06c6a770763cdefd94
horizon-sports-education <79442408+horizonsports@users.noreply.github.com>
2021-11-07 17:24:29 -0800

    usapage
    second

---
+++ docs/index.js
@@ -0,0 +1,25 @@
+function tz() {
+  window.location.href = "about.html";
+}
+
+function tzE() {
+  window.location.href = "aboutE.html";
+}
+
+function tzhome() {
+  window.location.href = "index.html";
+}
+
+function cheers() {
+  window.location.href = "https://www.cheers.com.tw/career/article/5100044";
+}
+
+function shane2() {
+  window.location.href =
+    "https://running.biji.co/index.php?q=news&act=info&id=96384&subtitle=【焦點人物】期待東山再起%20%2050蝶紀錄保持人－周校永";
+}
+
+function mitzi2() {
+  window.location.href =
+    "https://newbloommag.net/2017/07/18/ting-universiade-selection-sport-reform/";
+}
